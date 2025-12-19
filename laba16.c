ed7653fd7914658b1229e0872e47592896d0d5b9
elisonehl18-hub <elisonehl18@gmail.com>
2025-12-19 18:02:35 +0000

    laba16.c

---
+++ laba16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int sum = 0;
+    scanf("%s", str);
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] >= '0' && str[i] <= '9') {
+            sum += str[i] - '0';
+        }
+    }
+    printf("%d", sum);
+    return 0;
+}
